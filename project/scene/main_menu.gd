979abe2525ba594217117217714cae97bcdb54e3
Khoi <115506492+Mouse1456@users.noreply.github.com>
2025-10-23 19:29:46 -0700

    main menu updated

--- project/scene/main_menu.gd
+++ project/scene/main_menu.gd
@@ -1,12 +1,12 @@
 extends Control
 
+@onready var main_menu_buttons: VBoxContainer = $main_menu_buttons
+@onready var settings_panel: Panel = $settings_panel
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	main_menu_buttons.visible = true
+	settings_panel.visible = false
 
 # Start button function
 func _on_start_pressed() -> void:
@@ -14,8 +14,12 @@ func _on_start_pressed() -> void:
 	
 # Settings button function
 func _on_settings_pressed() -> void:
-	pass # Replace with function body.
+	main_menu_buttons.visible = false
+	settings_panel.visible = true
 
 # Exit button function
 func _on_exit_pressed() -> void: 
 	get_tree().quit() # Quit the game
+
+func _on_back_pressed() -> void:
+	_ready()
